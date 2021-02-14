1fe3017ae4d22536feed42edc3304bc1117d3d3f
root <root@Lakshmi.localdomain>
2021-02-14 15:58:42 -0300

    aplicando redux

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,16 +1,30 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
+import { setFavorite } from '../actions'
 import '../assets/styles/components/CarouselItem.scss'
 import playIcon from '../assets/statics/play-icon.png'
 import plusIcon from '../assets/statics/plus-icon.png'
 
-const CarouselItem = ({ cover, title, year, contentRating, duration }) => (
+const CarouselItem = () => {
+    const { cover, title, year, contentRating, duration } = props
+    handleSetFavorite = () => {
+        props.setFavorite({
+            cover, title, year, contentRating, duration,
+        })
+    }
+    return (
     <div className="carousel-item">
     <img className="carousel-item__img" src={cover} alt={title} />
     <div className="carousel-item__details">
         <div>
             <img className="carousel-item__details--img" src={playIcon} alt="Play" />
-            <img className="carousel-item__details--img" src={plusIcon} alt="Plus" />
+            <img 
+            className="carousel-item__details--img" 
+            src={plusIcon} 
+            alt="Plus" 
+            onClick={handleSetFavorite}
+            />
         </div>
         <p className="carousel-item__details--title">{title}</p>
         <p className="carousel-item__details--subtitle">
@@ -18,7 +32,7 @@ const CarouselItem = ({ cover, title, year, contentRating, duration }) => (
         </p>
     </div>
 </div>
-)
+)}
 
 CarouselItem.propTypes = {
     cover: PropTypes.string,
@@ -28,5 +42,10 @@ CarouselItem.propTypes = {
     duration: PropTypes.number,
 }
 
-export default CarouselItem
+const mapDispatchToProps = {
+    setFavorite,
+}
+
+
+export default connect(null, mapDispatchToProps)(CarouselItem)
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 import '../assets/styles/components/Header.scss'
 import Logo from '../assets/statics/logo-platzi-video-BW2.png'
 import userIcon from '../assets/statics/user-icon.png'
@@ -7,15 +8,18 @@ import userIcon from '../assets/statics/user-icon.png'
 const Header = () => {
     return (
         <header className="header">
-        <img className="header__img" src={Logo} alt="User" />
+
+        <Link to="/">
+            <img className="header__img" src={Logo} alt="User" />
+        </Link>       
         <div className="header__menu">
             <div className="header__menu--profile">
                 <img className="header__menu--img" src={userIcon} alt="image icon"/>
                 <p>Perfil</p>
             </div>
             <ul>
-                <li><a href="iniciar-secion.html">Cuenta</a></li>
-                <li><a href="/">Cerrar Seción</a></li>
+                <li><Link to="/">Cuenta</Link></li>
+                <li><Link to="/login">Iniciar Seción</Link></li>
 
             </ul>
         </div>
