e8cf9ef8e562972eef9d172728f081d3fe792da8
root <root@Lakshmi.localdomain>
2021-04-11 01:39:28 -0300

    remove stripe and materialize-css from components

--- src/routes/App.js
+++ src/routes/App.js
@@ -6,8 +6,6 @@ import Register from '../containers/Register'
 import Player from '../containers/Player'
 import NotFound from '../containers/NotFound'
 import Layout from '../components/Layout'
-import PayForm from '../containers/PayForm'
-import 'materialize-css/dist/css/materialize.min.css'
 
 
 const App = () => (
@@ -18,7 +16,6 @@ const App = () => (
                 <Route exact path="/login" component={Login} />
                 <Route exact path="/register" component={Register} />
                 <Route exact path="/player/:id" component={Player} />
-                <Route exact path="/payform" component={PayForm} />
                 <Route component={NotFound} />
             </Switch>
         </Layout>
