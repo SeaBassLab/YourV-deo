79893a8ef8026769acc94c77602991815ff02db2
root <root@Lakshmi.localdomain>
2021-04-13 10:16:32 -0300

    Hydrate y estado de Redux desde Express

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -6,15 +6,16 @@ import { Router } from 'react-router';
 import { createBrowserHistory } from 'history';
 import reducer from './reducers';
 import App from './routes/App';
-import initialState from './initialState';
-   
 
 
 const history = createBrowserHistory();
+const preloadedState = window.__PRELOADED_STATE__;
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-const store = createStore(reducer, initialState, composeEnhancers())
+const store = createStore(reducer, preloadedState, composeEnhancers())
 
-ReactDom.render(
+delete window.__PRELOADED_STATE__;
+
+ReactDom.hydrate(
     <Provider store={store}>
       <Router history={history}>
         <App />
--- src/server/index.js
+++ src/server/index.js
@@ -6,7 +6,7 @@ require('@babel/register')({
 
 require('asset-require-hook')({
     extensions: ['jpg', 'png', 'gift'],
-    name: '/assets/[hash].[ext]',
+    name: 'assets/[hash].[ext]',
 });
 
 require('./server');
